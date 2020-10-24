c3ab7a35c739d111b3dcb9c0ff6a0d9747676dcc
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2020-10-24 22:14:22 +0100

    Loader added

--- views/src/js/main.js
+++ views/src/js/main.js
@@ -20,3 +20,21 @@
 // } else {
 //     console.log(`Service worker not supported`);
 // }
+
+const containerLoad = document.querySelector(".loader");
+
+function HideLoader() {
+    containerLoad.classList.toggle("zero-opacity");
+    setTimeout(() => {
+        containerLoad.classList.toggle("hide");
+    }, 350);
+}
+
+function ShowLoader() {
+    containerLoad.classList.toggle("zero-opacity");
+    containerLoad.classList.toggle("hide");
+}
+
+window.onload = function(){
+    HideLoader();
+}
