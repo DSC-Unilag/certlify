48eb110188df6ea8fa14dad76bfe1bd289a3d858
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2020-10-29 22:09:24 +0100

    service worker working

--- views/src/js/main.js
+++ views/src/js/main.js
@@ -1,27 +1,3 @@
-
-// // if (!('indexedDB' in window)) {
-// //     console.log(`This browser doesn't support IndexedDB`);
-// // }
-
-// // if ('function' === typeof importScripts) {
-// //     importScripts(`older-browser-support/cache-polyfill/index.js`);
-// // }
-
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener("load", () => {
-//         navigator.serviceWorker.register("sw.js")
-//             .then(reg => {
-//                 console.log(`Service worker registrated: ${reg}`);
-//             })
-//             .catch(err => {
-//                 console.log(`Service worker registration error: ${err}`);
-//             })
-//     })
-// } else {
-//     console.log(`Service worker not supported`);
-// }
-
-
 const containerLoad = document.querySelector(".loader");
 
 function HideLoader() {
