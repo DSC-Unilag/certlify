b5a37697ba578fb28af7ebe211c9a1cebbfbf081
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2020-10-31 00:07:36 +0100

    service worker fixed

--- views/sw.js
+++ views/sw.js
@@ -52,16 +52,8 @@ self.addEventListener("activate", event => {
 
 self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.open(fileVersion).then(function (cache) {
-            return cache.match(event.request).then(function (response) {
-                return (
-                    response ||
-                    fetch(event.request).then(function (response) {
-                        cache.put(event.request, response.clone());
-                        return response;
-                    })
-                );
-            });
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
         }),
     );
 });
