fa504474defd30bf1cf059407b069ddea53b91bf
Bolubee101 <akinwandeakinboluwarin@gmail.com>
2020-11-02 02:56:50 +0100

    fixed dashboard content number inconsistency

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   database:
     "mongodb+srv://young:ethene20@cluster0.hgbre.mongodb.net/tester?retryWrites=true&w=majority",
-    //"mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false",
+   // "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false",
   secret: "secret-key",
   rounds:10
 };
--- controllers/GetDashboard.js
+++ controllers/GetDashboard.js
@@ -4,53 +4,51 @@ const Links = require("../models/links");
 // controller to serve the dashboard data for a logged in user
 let dashboard = (req, res) => {
     let email
-    if(req.session.email){
+    if (req.session.email) {
         email = req.session.email
-    }else{
+    } else {
         email = req.session.anon
     }
-    
+
     if (email) {
-        
+
         User.findOne({ email }, function (err, user) {
-            let data={}
+            let data = {}
             if (user) {
                 data.name = user.name
                 data.certificateUrls = user.certificateUrls
-                data.certs= [];
+                data.certs = [];
                 let links = user.certificateUrls
-                const loop=async ()=>{
-                    for (i in links) {
-                        data.certs[i]={}
-                         await Links.findOne({ link: links[i] }, function (err, cert) {
-                            if (cert) {
-                                data.certs[i].name = cert.name
-                                data.certs[i].link = cert.link
-                                data.certs[i].thumb=cert.thumb
-                                data.certs[i].boundary = cert.boundary
-                            }
-                        })
+
+                Links.find({
+                    link: { $in: links }
+                }, function (err, docs) {
+                    for (i in docs) {
+                        data.certs[i] = {};
+                        data.certs[i].name = docs[i].name
+                        data.certs[i].link = docs[i].link
+                        data.certs[i].thumb = docs[i].thumb
+                        data.certs[i].boundary = docs[i].boundary
                     }
                     res.json(data);
-                }
-                loop();
-               
-            }else{
-               
+                });
+
+            } else {
+
                 res.status(401)
                 res.json({
-                    status:false,
-                    message:"Unregistered user"
+                    status: false,
+                    message: "Unregistered user"
                 })
             }
         })
     } else {
         res.status(401)
         res.json({
-            status:false,
-            message:"user not signed in"
+            status: false,
+            message: "user not signed in"
         })
     }
 
 }
-module.exports=dashboard;
+module.exports = dashboard;
