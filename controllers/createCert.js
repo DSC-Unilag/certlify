9275b039940c30db3ce165fcda1857df572f5865
Bolubee101 <akinwandeakinboluwarin@gmail.com>
2020-11-08 13:57:32 +0100

    cloudinary integration

--- controllers/createCert.js
+++ controllers/createCert.js
@@ -2,6 +2,13 @@
 const User = require("../models/users");
 var uniqid = require('uniqid');
 const Link = require("../models/links");
+let cloudinary=require("cloudinary").v2
+
+// cloudinary.config({
+//     cloud_name:"de5neer84",
+//     api_key:"488273542881848",
+//     api_secret:"0dhjwq6UXnVpIzkMSaBI9sQCEws"
+// })
 
 let certificate = (req, res) => {
     if (!req.session.email&&!req.session.anon) {
@@ -25,23 +32,26 @@ let certificate = (req, res) => {
                 user.save((err) => {
                     if (err) throw err
                     else {
-                        let cert = new Link({
-                            issuer: req.session.email||req.session.anon,
-                            name: req.body.name,
-                            link: link,
-                            src:req.body.src,
-                            thumb:req.body.thumb,
-                            boundary: req.body.boundary,
-                        })
-                        cert.save(function (err, doc) {
-                            if (err) return console.error(err);
-                            res.json({
-                                status: true,
-                                message: "certificate created",
-                                link: req.hostname + "/certificate/" + link,
-                                url:link
+                        cloudinary.uploader.upload(req.body.src,(error,result)=>{
+                            let cert = new Link({
+                                issuer: req.session.email||req.session.anon,
+                                name: req.body.name,
+                                link: link,
+                                src:result.url,
+                                thumb:req.body.thumb,
+                                boundary: req.body.boundary,
                             })
-                        });
+                            cert.save(function (err, doc) {
+                                if (err) return console.error(err);
+                                res.json({
+                                    status: true,
+                                    message: "certificate created",
+                                    link: req.hostname + "/certificate/" + link,
+                                    url:link
+                                })
+                            });
+                        })
+                       
                     }
                 })
             }
