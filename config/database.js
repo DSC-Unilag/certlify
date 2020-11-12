c0f87f04368d7333c2b2fe6dd968e00b6600f28c
Bolubee101 <akinwandeakinboluwarin@gmail.com>
2020-11-12 20:51:23 +0100

    Updated footer

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // This should be your own database url
 module.exports = {
   database:
-    "",
+    "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false",
   secret: "secret-key",
   rounds:10,
   email:"",
