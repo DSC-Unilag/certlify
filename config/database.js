e884938788da8eb74ce04f88c78c09321f677a11
Bolubee101 <akinwandeakinboluwarin@gmail.com>
2020-11-12 15:40:30 +0100

    removed the mongo uri

--- config/database.js
+++ config/database.js
@@ -1,8 +1,7 @@
 // This should be your own database url
 module.exports = {
   database:
-    "mongodb+srv://young:ethene20@cluster0.hgbre.mongodb.net/tester?retryWrites=true&w=majority",
-   // "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false",
+    "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false",
   secret: "secret-key",
   rounds:10,
   email:"",
