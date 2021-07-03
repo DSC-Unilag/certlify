87dd2ec15bfb469eae7572780416c2aed0d895f8
Zubs <zubairidrisaweda@gmail.com>
2021-07-03 17:25:45 +0100

    404

--- src/components/apiRouter.js
+++ src/components/apiRouter.js
@@ -1,35 +1,14 @@
-const express = require('express');
-const apiRouter = express.Router();
+const express = require('express')
+const apiRouter = express.Router()
 
-const authroutes = require("./auth/auth.router");
-apiRouter.use('/auth', authroutes);
+const authroutes = require("./auth/auth.router")
+apiRouter.use('/auth', authroutes)
 
-apiRouter.get('*', (req, res) => {
-	res.status(404);
+apiRouter.use((err, req, res, next) => {
+	res.status(404)
 	return res.json({
 		errorMessage: 'endpoint not found',
-	});
-});
+	})
+})
 
-apiRouter.post('*', (req, res) => {
-  res.status(404);
-  return res.json({
-    errorMessage: 'endpoint not found',
-  });
-});
-
-apiRouter.put('*', (req, res) => {
-  res.status(404);
-  return res.json({
-    errorMessage: 'endpoint not found',
-  });
-});
-
-apiRouter.delete('*', (req, res) => {
-  res.status(404);
-  return res.json({
-    errorMessage: 'endpoint not found',
-  });
-});
-
-module.exports = apiRouter;
+module.exports = apiRouter
