30ddf89c3f201672747c76dbd854e71ec5916a43
Zubs <zubairidrisaweda@gmail.com>
2021-07-03 17:18:39 +0100

    Login

--- src/components/auth/auth.router.js
+++ src/components/auth/auth.router.js
@@ -1,29 +1,10 @@
 const express = require('express');
-const auth=require("./auth.controller");
-
-const {
-    generateResponse,
-    createError,
-    createSuccessMessage,
-  } = require("../../utils/response");
-
+const { register } = require("./registerController.ctrl");
 
 const router = express.Router();
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
 
-router.get('/login',auth.login);
+router.get('/register', register);
 
-module.exports=router;
-
-// using the response utils, error
-/*
-result = generateResponse(400, createError(err.message));
-    return res.status(result.status).json(result.result);
-*/
-
-// using the response utils, success
-/*
-result = generateResponse(200, createSuccessMessage({ token, user }));
-    return res.status(result.status).json(result.result);
-*/
+module.exports = router;
---
+++ src/components/auth/loginController.ctrl.js
@@ -0,0 +1,37 @@
+// Import modules
+const { compare } = require("bcrypt")
+const { User } = require("../users/users.model")
+const { createToken } = require("../../utils/token")
+const { sendAuthError } = require("../../utils/sendErrors")
+const { generateResponse, createError, createSuccessMessage } = require("../../utils/response")
+
+export async function Login (req, res) {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.findOne({ email })
+        
+        if (user) {
+            const auth = await compare(password, user.password)
+
+            if (auth) {
+                const token = await createToken(user._id)
+                const result = generateResponse(201, createSuccessMessage({ user, token: token.value }))
+                return res.status(result.status).json(result.result)
+            } else {
+                const errors = sendAuthError({}, true)
+                const result = generateResponse(401, createError(errors))
+                return res.status(result.status).json(result.result)
+            }
+        } else {
+            const result = generateResponse(404, createError({
+                message: "User Not Found"
+            }))
+            return res.status(result.status).json(result.result)
+        }
+    } catch (error) {
+        const errors = sendAuthError(error)
+        const result = generateResponse(401, createError(errors))
+        return res.status(result.status).json(result.result)
+    }
+}
