e82454237de4917d2d853616d6297d1e7ed30472
Zubs <zubairidrisaweda@gmail.com>
2021-09-18 12:27:15 +0100

    Start refactoring

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors=require("cors");
+const cors = require("cors");
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 const logger = require('morgan')
