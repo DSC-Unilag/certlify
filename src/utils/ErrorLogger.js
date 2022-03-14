e8ecfde80e0279cb98322ddcc536bcccdb4a24ef
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-03-14 13:46:14 +0100

    Updated logger to use timestamps

--- src/utils/ErrorLogger.js
+++ src/utils/ErrorLogger.js
@@ -1,13 +1,33 @@
 // Import dependencies
 const winston = require('winston');
 const path = require('path');
+const DailyRotateFile = require('winston-daily-rotate-file');
+const MESSAGE = Symbol.for('message');
+
+const jsonFormatter = (logEntry) => {
+    const base = { timestamp: new Date() };
+    const json = Object.assign(base, logEntry)
+    logEntry[MESSAGE] = JSON.stringify(json);
+    return logEntry;
+}
 
 exports.FileLogger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
+    format: winston.format(jsonFormatter)(),
     transports: [
         new winston.transports.File({
             filename: path.join(__dirname, '..', 'log', 'error.log'),
+            level: 'error'
+        }),
+        new DailyRotateFile({
+            filename: path.join(__dirname, '..', 'log', 'status-%DATE%.log'),
+            datePattern: 'YYYY-MM-DD-HH',
+            maxSize: '20m',
+        }),
+        new DailyRotateFile({
+            filename: path.join(__dirname, '..', 'log', 'error-%DATE%.log'),
+            datePattern: 'YYYY-MM-DD-HH',
+            maxSize: '20m',
             level: 'error'
         }),
         new winston.transports.File({
